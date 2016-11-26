37979435506468668edc5c0948462da956f7fe6b
Shankar Kulumani <shanks.k@gmail.com>
2016-11-26 16:37:37 -0500

    Add setup repo script

---
+++ setup_repo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# add the correct remote repositories
+git_repo="git@github.com:skulumani/2016_ACC_matlab.git"
+bb_repo="git@bitbucket.org:shankarkulumani/2016-acc-matlab.git"
+fdcl_repo="git@github.com:fdcl-gwu/2016_ACC_matlab.gitb"
+
+printf "Setting the origin remote to point to Github and Bitbucket:\n\n"
+printf "Github: $git_repo\n"
+printf "Bitbucket: $bb_repo\n"
+printf "FDCL: $fdcl_repo\n\n"
+
+git remote add origin $git_repo
+git remote set-url origin --push --add $bb_repo
+git remote set-url origin --push --add $git_repo
+git remote set-url origin --push --add $fdcl_repo
+
+printf "Finished! The new remotes are listed.\n"
+printf "\n"
+
+git remote -v
