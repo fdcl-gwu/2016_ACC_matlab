8e76e14f235d628a219b09a5842f469033c2fdce
Shankar Kulumani SEAS-Linux <skulumani@gwmail.gwu.edu>
2016-12-28 16:25:21 -0500

    Start of error functino visualization

---
+++ error_function_visualization.m
@@ -0,0 +1,17 @@
+% 28 Dec 16 
+% Visulalize error function behavior to try and find some relationship for
+% lypanov stability proof
+
+% anonomous functions for config error terms
+
+% create surface plot of \Psi = A B and e_{R}
+
+% loop over angle between R^T v and r
+
+% loop over angle between R and R_D (using Rodriguez formula)
+
+% store height into arrays
+
+% create surface
+
+
